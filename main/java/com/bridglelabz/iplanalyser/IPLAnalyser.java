1150cc6dd4a2b50ed5c399a4ba8df5bbc01dfa42
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-11-08 23:29:52 +0530

    Refactored : code for creating method for loading and sorting MostWicket class

--- main/java/com/bridglelabz/iplanalyser/IPLAnalyser.java
+++ main/java/com/bridglelabz/iplanalyser/IPLAnalyser.java
@@ -6,10 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
-import java.util.stream.StreamSupport;
-
 import com.bridgelabz.csvreader.CSVBuilderFactory;
 import com.bridgelabz.csvreader.CensusAnalyserException;
 import com.bridgelabz.csvreader.ICSVBuilder;
@@ -20,9 +17,11 @@ import com.google.gson.Gson;
 public class IPLAnalyser {
 
 	List<MostRuns> runsCSVList = null ;
+	List<MostWickets> wicketsCSVList = null ;
 
 	public IPLAnalyser() {
-		this.runsCSVList = new ArrayList<MostRuns>();
+		this.runsCSVList = new ArrayList<MostRuns>() ;
+		this.wicketsCSVList = new ArrayList<MostWickets>() ;
 	}
 
 	public int loadData(String csvFile) throws CensusAnalyserException 
@@ -33,6 +32,23 @@ public class IPLAnalyser {
 			ICSVBuilder csvBuilderFactory = CSVBuilderFactory.createCSVBuilder();
 			runsCSVList = csvBuilderFactory.getCSVFileList(reader, MostRuns.class) ;
 			return runsCSVList.size() ;
+		} catch (IOException e) {
+			throw new CensusAnalyserException("Please enter correct path",
+                    					CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+		}catch (RuntimeException e) {
+            		throw new CensusAnalyserException("Please select correct csv file  ",
+                    					CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES);
+		}
+	}
+	
+	public int loadWicketData(String wicketCSVFile) throws CensusAnalyserException 
+	{
+		try
+		{
+			Reader reader = Files.newBufferedReader(Paths.get(wicketCSVFile));
+			ICSVBuilder csvBuilderFactory = CSVBuilderFactory.createCSVBuilder();
+			wicketsCSVList = csvBuilderFactory.getCSVFileList(reader, MostWickets.class) ;
+			return wicketsCSVList.size() ;
 		} catch (IOException e) {
 			throw new CensusAnalyserException("Please enter correct path",
                     					CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
@@ -129,5 +145,39 @@ public class IPLAnalyser {
                     					CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES);
 		}	
 		
-	}	
+	}
+
+	public String givenBestBowlingAverageSort(String wicketCSVFile) throws CensusAnalyserException {
+		try {
+			loadWicketData(wicketCSVFile);
+		 if (wicketsCSVList == null || wicketsCSVList.size() == 0) {
+	            throw new CensusAnalyserException("NO_CENSUS_DATA", CensusAnalyserException.ExceptionType.UNABLE_TO_PARSE);
+	        }			
+			Comparator<MostWickets> iplWicketComparator = Comparator.comparing(iplWicket -> iplWicket.bowlingAverage) ;
+			this.WicketSort(iplWicketComparator);
+		    String sortedWicketsJson = new Gson().toJson(this.wicketsCSVList);
+		    return sortedWicketsJson;
+			
+		} catch (RuntimeException e) {
+         		throw new CensusAnalyserException("Please select correct csv file  ",
+                 					CensusAnalyserException.ExceptionType.CSV_FILE_INTERNAL_ISSUES);
+		}	
+	}
+	
+
+	private void WicketSort(Comparator<MostWickets> iplWicketComparator) {
+		for (int index = 0; index < wicketsCSVList.size(); index++) {
+            for (int j_increment = 0; j_increment < wicketsCSVList.size() - index - 1; j_increment++) {
+                MostWickets firtRunsObject = wicketsCSVList.get(j_increment);
+                MostWickets nextRunsObject = wicketsCSVList.get(j_increment + 1);
+                if (iplWicketComparator.compare(firtRunsObject, nextRunsObject) < 0) {
+                	wicketsCSVList.set(j_increment, nextRunsObject);
+                	wicketsCSVList.set(j_increment + 1, firtRunsObject);
+                }
+
+            }
+
+        }
+		
+	}
 }
